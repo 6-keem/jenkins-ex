dba09bb7bab2dc75a80df1383c5bb2e2fbb687d7
6-keem <6ukeem@gmail.com>
2025-02-09 00:32:35 +0900

    Initial commit

---
+++ src/main/java/com/jenkins/jenkinsex/JenkinsExApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkinsex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsExApplication.class, args);
+	}
+
+}
